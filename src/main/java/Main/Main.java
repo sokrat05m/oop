7c135eaaf562ecd7bef27782ca2a74c8c1fb226d
Sokrat Magomedov <oooosokrat@mail.ru>
2024-02-04 12:09:01 +0300

    Homework #2 red

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -10,20 +10,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Owner sokrat = new Owner("Сократ");
-        Illness rana = new Illness("Рана ноги");
-        List<Goable> lst = new ArrayList<>();
-        Cat ketu = new Cat("Гъаба", sokrat, LocalDate.now(), rana, 10D);
-        Dog hoy = new Dog();
-        lst.add(ketu);
-        lst.add(hoy);
 
-        Doctor doc = new Doctor();
-        doc.cure(ketu);
-        VetClinic clinic = new VetClinic();
-        clinic.addPatients(ketu);
-        System.out.println(clinic.getPatients());
-        clinic.addPersonal(doc);
-        System.out.println(clinic.getPersonal());
     }
 }
